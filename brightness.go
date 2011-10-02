81a75f788591056fcc4caf08a88d993eff313fb8
Kannan <manki@users.noreply.github.com>
2011-10-02 14:52:44 +1100

    Added brightness.go, a program that changes LCD brightness using setpci command.

---
+++ brightness.go
@@ -0,0 +1,47 @@
+// Go program to run "screen brightness" commands.
+
+package main
+
+import (
+	"exec"
+	"fmt"
+	"os"
+)
+
+func brightness() int {
+	cmd := exec.Command("setpci", "-s", "00:02.0", "f4.b")
+	out, err := cmd.Output()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Running setpci failed: %v\n", err)
+		os.Exit(-1)
+	}
+
+	var b int
+	fmt.Sscanf(string(out), "%x", &b)
+	return b
+}
+
+func setBrightness(b int) {
+	cmd := exec.Command("setpci", "-s", "00:02.0", fmt.Sprintf("f4.b=%x", b))
+	if err := cmd.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Running setpci failed: %v\n", err)
+		os.Exit(-1)
+	}
+}
+
+func main() {
+	switch len(os.Args) {
+	case 1:
+		fmt.Println(brightness())
+	case 2:
+		var delta int
+		fmt.Sscanf(os.Args[1], "%d", &delta)
+		setBrightness(brightness() + delta)
+	default:
+		fmt.Fprintf(os.Stderr, "Usage: %s [delta]\n\n", os.Args[0])
+		fmt.Fprintln(os.Stderr, "Without arguments prints current brightness level")
+		fmt.Fprintln(os.Stderr, "With delta changes brightness")
+		fmt.Fprintln(os.Stderr, "brightness 50 increases brightness by 50")
+		fmt.Fprintln(os.Stderr, "brightness -50 decreases brightness by 50")
+	}
+}
